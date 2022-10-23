5fb9823b20ff5b1a37695ff02f57085cc2cf3e10
JamesSCrank <jscrank@cpp.edu>
2022-10-22 20:54:54 -0700

    Start

---
+++ main.c
@@ -0,0 +1,6 @@
+//need to figure out how to use command line arguments in the program 
+//program needs to use four different preset menu items 
+//and randomly call upon one of these items before continuing on
+//must use command line arguments to calculate the tips and taxes
+//Will use local tax 8.75 and tip percentage of 15%
+
